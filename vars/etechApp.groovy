5d53dbbc4ca6e645073865ebe4f1e76521d223bf
olusolasp <127486644+olusolasp@users.noreply.github.com>
2023-04-06 19:22:30 -0700

    Update etechApp.groovy

--- vars/etechApp.groovy
+++ vars/etechApp.groovy
@@ -1,1 +1,24 @@
+def call(String repoUrl){
+    pipeline {
+       agent any
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'lscpu'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'df -h'
+               }
+           }
+       }
+}
 
